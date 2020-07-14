d1db9f65905c53ca01e03ff57f766ad570f96cc6
liaoms <763905926@qq.com>
2020-07-14 14:28:21 +0800

    更改线程信号对应槽函数所依附的线程

--- 12_SignalInThread/SignalInThread/main.cpp
+++ 12_SignalInThread/SignalInThread/main.cpp
@@ -9,8 +9,12 @@ int main(int argc, char *argv[])
 
     qDebug() << "main tid = " << QThread::currentThreadId();
 
-    TestThread t;
-    MyObject m;
+    TestThread t;   //线程t中槽函数的依附线程默认为主线程(main线程)
+    MyObject m;     //同上(即槽函数实际运行位置main线程中)
+
+    //线程信号对应的槽函数，应该运行在对应线程的栈中，运行到其他线程中，临界资源访问可能出错
+    m.moveToThread(&t);     //将m中槽函数的依附线程设置为线程t
+    t.moveToThread(&t);     //将线程t中槽函数的依附线程设置为线程t
 
     QObject::connect(&t, SIGNAL(started()), &m, SLOT(threadStartSlot()));   //关联线程started()信号
     QObject::connect(&t, SIGNAL(finished()), &m, SLOT(threadFinishSlot())); //关联线程finished()信号
