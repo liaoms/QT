23833a6c49461985d5cd404f8ab3371f1887f154
liaoms <763905926@qq.com>
2020-07-14 14:18:53 +0800

    线程中定义信号与槽;关联线程信号到其他对象的槽函数

---
+++ 12_SignalInThread/SignalInThread/TestThread.cpp
@@ -0,0 +1,30 @@
+﻿#include "TestThread.h"
+#include <QDebug>
+
+TestThread::TestThread(QObject* parent) : QThread(parent)
+{
+
+    connect(this, SIGNAL(testsignal()), this, SLOT(testSlot()));
+}
+
+void TestThread::run()
+{
+    qDebug() << " thread begin";
+    qDebug() << "TestThread tid = " << currentThreadId();
+    int i=5;
+    while(i--)
+    {
+        qDebug() << " thread run...";
+        sleep(1);
+    }
+    emit testsignal();
+
+    qDebug() << " thread end";
+}
+
+
+void TestThread::testSlot()
+{
+    qDebug() << "testSlot tid = " << currentThreadId();
+    qDebug() << "TestThread::testSlot()";
+}
---
+++ 12_SignalInThread/SignalInThread/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <QCoreApplication>
+#include "TestThread.h"
+#include "MyObject.h"
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    qDebug() << "main tid = " << QThread::currentThreadId();
+
+    TestThread t;
+    MyObject m;
+
+    QObject::connect(&t, SIGNAL(started()), &m, SLOT(threadStartSlot()));   //关联线程started()信号
+    QObject::connect(&t, SIGNAL(finished()), &m, SLOT(threadFinishSlot())); //关联线程finished()信号
+
+    t.start();
+    return a.exec();
+}
